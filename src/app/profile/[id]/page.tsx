52c4e119042119908ac444612da01e32d59ecab1
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-10-11 16:44:26 +0530

    first commit

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfilePage = () => {
+  return (
+    <div>pagedynamic</div>
+  )
+};
+
+export default ProfilePage;
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>pageactual</div>
+  )
+}
+
+export default page
